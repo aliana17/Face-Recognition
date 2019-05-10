a4094ced24af1f3396c9959ecfc74406ba171bd5
änchal7299 <agarwalanchal72@gmail.com>
2019-05-10 21:45:08 +0530

    First Commit

---
+++ Model.py
@@ -0,0 +1,93 @@
+import cv2
+import numpy as np
+import webbrowser
+import requests
+from os import listdir
+from os.path import isfile, join
+
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+print(cv2.__version__)
+# Get the training data we previously made
+data_path = '/root/new/'
+
+onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
+
+# Create arrays for training data and labels
+Training_Data, Labels = [], []
+
+# Open training images in our datapath
+# Create a numpy array for training data
+for i, files in enumerate(onlyfiles):
+    image_path = data_path + onlyfiles[i]
+    images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    Training_Data.append(np.asarray(images, dtype=np.uint8))
+    Labels.append(i)
+# 
+# Create a numpy array for both training data and labels
+Labels = np.asarray(Labels, dtype=np.int32)
+model=cv2.face_LBPHFaceRecognizer.create()
+
+# Let's train our model 
+model.train(np.asarray(Training_Data), np.asarray(Labels))
+print("Model trained sucessefully")
+
+def face_detector(img, size=0.5):
+    
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if faces is ():
+        return img, []
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),2)
+        roi = img[y:y+h, x:x+w]
+        roi = cv2.resize(roi, (200, 200))
+    return img, roi
+
+
+# Open Webcam
+url = "https://192.168.43.1:8080/shot.jpg"
+
+while True:
+
+    data = requests.get(url)
+    n = data.content
+    imageArray = bytearray(n)
+    imageID = np.array(imageArray)
+    img = cv2.imdecode(imageID, -1)
+    face_coord = face_classifier.detectMultiScale(img)
+    
+    image, face = face_detector(face_coord)
+    
+    try:
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+        # Pass face to prediction model
+        # "results" comprises of a tuple containing the label and the confidence value
+        results = model.predict(face)
+        print(results)
+        if results[1] < 500:
+            confidence = int( 100 * (1 - (results[1])/400) )
+            display_string = str(confidence) + '% Confident it is User'
+            
+        cv2.putText(image, display_string, (100, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (255,120,150), 2)
+        
+        if confidence > 75:
+            cv2.putText(image, "Hey Vivek", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+            cv2.imshow('Face Recognition', image )
+                       
+        else:
+            cv2.putText(image, "Locked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+            cv2.imshow('Face Recognition', image )
+
+    except:
+        cv2.putText(image, "No Face Found", (220, 120) , cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+        cv2.putText(image, "Locked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+        cv2.imshow('Face Recognition', image )
+        pass
+        
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+        break
+        
+cv2.destroyAllWindows() 
